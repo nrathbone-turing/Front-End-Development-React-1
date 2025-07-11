2153055613025d11f56a682d899827b8fcaf9f7d
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-11 13:43:57 -0600

    modify Links.js to render user links dynamically

--- 2_react_components/technical-lesson-components-props/src/components/Links.js
+++ 2_react_components/technical-lesson-components-props/src/components/Links.js
@@ -1,9 +1,12 @@
 import React from "react";
-function ComponentName() {
+
+function Links({ github, linkedin }) {
   return (
-    <div id="component-name">
-      Component Content
+    <div>
+      <h3>Links</h3>
+      <a href={github}>{github}</a>
+      <a href={linkedin}>{linkedin}</a>
     </div>
   );
 }
-export default ComponentName;
+export default Links;
