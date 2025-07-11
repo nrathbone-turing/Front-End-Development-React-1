04726073b67b2d76750926986e07a6d9fdec7f39
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-11 13:51:02 -0600

    modify About.js to include the Links component

--- 2_react_components/technical-lesson-components-props/src/components/About.js
+++ 2_react_components/technical-lesson-components-props/src/components/About.js
@@ -1,9 +1,14 @@
 import React from "react";
-function ComponentName() {
+import Links from "./Links";
+
+function About({ bio, links }) {
   return (
-    <div id="component-name">
-      Component Content
+    <div id="about">
+      <h2>About Me</h2>
+      <p>{bio}</p>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links github={links.github} linkedin={links.linkedin} />
     </div>
   );
 }
-export default ComponentName;
+export default About;
