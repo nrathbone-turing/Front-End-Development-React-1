6577ad0e2b1c0090a2ec5c4d89688b15fd8e64a4
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-11 13:50:12 -0600

    modify Home.js to accept props for color, name, and city

--- 2_react_components/technical-lesson-components-props/src/components/Home.js
+++ 2_react_components/technical-lesson-components-props/src/components/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
-function ComponentName() {
+function Home({ color, name, city }) {
   return (
-    <div id="component-name">
-      Component Content
+    <div id="home">
+      <h1 style={{ color: color }}>
+        {name} is a Web Developer from {city}
+      </h1>
     </div>
   );
 }
-export default ComponentName;
+export default Home;
