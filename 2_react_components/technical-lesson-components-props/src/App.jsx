a1962eae462bacd8552aa465dc1328ed9acd8f3b
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-11 14:07:53 -0600

    refactor App.jsx file and main.jsx file to correctly store App.jsx in the src directory instead of src/components; rename component files to use .jsx file extension for Vite's requirements

---
+++ 2_react_components/technical-lesson-components-props/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+import user from "./data/user";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} links={user.links} />
+    </div>
+  );
+}
+
+export default App;
