d49fdc12ff65e34f3f4b867206972025ccbe06af
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-12 21:01:58 -0600

    Update App.jsx to store the projects state, pass it to HomePage, and then pass addProject to AddProjectForm

--- 8_summative_assessment/project-showcase/src/App.jsx
+++ 8_summative_assessment/project-showcase/src/App.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import HomePage from "./pages/HomePage";
-import ProjectDetail from "./pages/ProjectDetail";
-import AddProjectForm from "./pages/AddProjectForm";
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import ProjectDetail from './pages/ProjectDetail';
+import AddProjectForm from './pages/AddProjectForm';
+import projectsData from './data/projects';
 
 function App() {
+  const [projects, setProjects] = useState(projectsData);
+
+  function addProject(newProject) {
+    setProjects([...projects, newProject]);
+  }
+
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/projects/:id" element={<ProjectDetail />} />
-        <Route path="/add" element={<AddProjectForm />} />
+        <Route path="/" element={<HomePage projects={projects}/>} />
+        <Route path="/projects/:id" element={<ProjectDetail projects={projects}/>} />
+        <Route path="/add" element={<AddProjectForm addProject={addProject}/>} />
       </Routes>
       <Footer />
     </>
