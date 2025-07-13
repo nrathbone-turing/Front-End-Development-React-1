bc445e9a2cf4094db7e417e20de5169e30c4ee11
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-12 20:20:51 -0600

    Update HomePage with description text

--- 8_summative_assessment/project-showcase/src/pages/HomePage.jsx
+++ 8_summative_assessment/project-showcase/src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ function HomePage() {
     <main>
       <section id="about">
         <h2>About Me</h2>
-        <p>Your blurb here...</p>
+        <p>I come from a support leadership role where I spent a lot of time bridging feedback to product and engineering. I realized I wanted to go beyond surface fixes and learn to build systemic solutions myself, so I joined my company’s internal apprenticeship program and enrolled in the Turing and Flatiron School bootcamps. I’m especially excited about applying what I know about UX pain points for internal and external users, as well as how to find and address edge cases, to my future of building more resilient tools.</p>
         <h3>Skills</h3>
         <ul>
           <li>HTML</li>
