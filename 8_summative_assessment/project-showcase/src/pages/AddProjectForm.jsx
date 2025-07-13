f6372678dce3b8683a63249abf958096d16121b1
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-12 20:50:14 -0600

    Refactor ProjectDetail to pass tests + Refactor AddProjectForm to have a skeleton placeholder

--- 8_summative_assessment/project-showcase/src/pages/AddProjectForm.jsx
+++ 8_summative_assessment/project-showcase/src/pages/AddProjectForm.jsx
@@ -1,1 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import projects from '../data/projects';
+
+function AddProjectForm() {
+  
+  return (
+    <>
+    
+    </>
+  );
+}
+
+export default AddProjectForm;
--- 8_summative_assessment/project-showcase/src/pages/ProjectDetail.jsx
+++ 8_summative_assessment/project-showcase/src/pages/ProjectDetail.jsx
@@ -1,1 +1,29 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import projects from '../data/projects';
+
+function ProjectDetail() {
+  const { id } = useParams();
+  const project = projects.find(p => p.id === parseInt(id));
+
+  if (!project) {
+    return <h2>Project not found</h2>;
+  }
+
+  return (
+    <main>
+      <h2>{project.title}</h2>
+      <img src={project.image} alt={project.alt || project.title} />
+      <p>{project.description}</p>
+      {project.link && (
+        <p>
+          <a href={project.link} target="_blank" rel="noreferrer">
+            View on GitHub
+          </a>
+        </p>
+      )}
+    </main>
+  );
+}
+
+export default ProjectDetail;
